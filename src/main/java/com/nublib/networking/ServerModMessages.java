4a4b47ac8d899c5eaf7e270a8c6ba67adedb1545
nub31 <oliverlhs@outlook.com>
2024-04-28 20:39:25 +0200

    Versioning

--- src/main/java/com/nublib/networking/ServerModMessages.java
+++
@@ -1,9 +0,0 @@
-package com.nublib.networking;
-
-import net.minecraft.util.Identifier;
-
-public class ServerModMessages {
-	public static Identifier createSynConfigIdentifier(String modId) {
-		return new Identifier(modId, "syn-config");
-	}
-}
