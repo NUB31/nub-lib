cfb1a2ad517c54308227b81c6d51f49f587b4dac
nub31 <oliverlhs@outlook.com>
2024-07-02 17:57:53 +0200

    Remove example config from client

--- src/client/java/com/nublib/NubLibClient.java
+++ src/client/java/com/nublib/NubLibClient.java
@@ -1,18 +1,9 @@
 package com.nublib;
 
-import com.nublib.config.provider.FileStorageProvider;
-import com.nublib.config.provider.IStorageProvider;
-import com.nublib.example.ExampleConfig;
-import com.nublib.util.BindingUtil;
 import net.fabricmc.api.ClientModInitializer;
-import org.lwjgl.glfw.GLFW;
 
 public class NubLibClient implements ClientModInitializer {
-    public static IStorageProvider STORAGE_PROVIDER = FileStorageProvider.create("example-config");
-    public static ExampleConfig CONFIG = new ExampleConfig(STORAGE_PROVIDER);
-
     @Override
     public void onInitializeClient() {
-        BindingUtil.bindScreenToKey(GLFW.GLFW_KEY_M, () -> CONFIG.createCustomScreen(null), "nub-lib.config.ui.key.show_example_config_screen_3");
     }
 }
